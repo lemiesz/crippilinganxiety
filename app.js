3885e85151816ffbda465b26b1bd8abe66a4b957
Robert Lemiesz <lemiesz@Roberts-MacBook-Pro.local>
2016-03-25 01:12:00 -0700

    my life

--- app.js
+++ app.js
@@ -8,15 +8,22 @@ window.onload = function() {
 	var height = body.offsetHeight;
 
 	
-		window.setInterval(function(){
-			var newElement = document.createElement("H1");
+	window.setInterval(function(){
+		var newElement = document.createElement("H1");
+		var randomChance = Math.random*100;
+		if(randomChance<2 && randomChance>10){
+			newElement.innerHTML = "why do i have no friends???";
+		} else if (randomChance < 15){
+			newElement.innerHTML = "fuck normies";
+		} else {
 			newElement.innerHTML = "end my life";
-			newElement.className = "sadText";
-			newElement.style.position = "absolute";
-			// newElement.style.position = "absolute"; 
-			newElement.style.right = (Math.random() * width);
-			newElement.style.top = (Math.random() * height);
-			body.appendChild(newElement);
-		}, 500);
+		}
+		newElement.className = "sadText";
+		newElement.style.position = "absolute";
+		// newElement.style.position = "absolute"; 
+		newElement.style.right = (Math.random() * width);
+		newElement.style.top = (Math.random() * height);
+		body.appendChild(newElement);
+	}, 500);
 	
 }
