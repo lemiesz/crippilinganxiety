17d1735e5a7479c1d88b514fd8e2affc36ce2df4
Robert Lemiesz <lemiesz@Roberts-MacBook-Pro.local>
2016-03-25 00:33:10 -0700

    my life is worthless

---
+++ app.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+	var element = document.getElementById("para1");
+	var body = document.body;
+	console.log(body)
+	console.log(element)
+
+	var width = body.offsetWidth;
+	var height = body.offsetHeight;
+
+	
+		window.setInterval(function(){
+			var newElement = document.createElement("H1");
+			newElement.innerHTML = "end my life";
+			newElement.className = "sadText";
+			newElement.style.position = "absolute";
+			// newElement.style.position = "absolute"; 
+			newElement.style.right = (Math.random() * width);
+			newElement.style.top = (Math.random() * height);
+			body.appendChild(newElement);
+		}, 500);
+	
+}
